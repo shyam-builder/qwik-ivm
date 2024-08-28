c6e53f8cf3d93a1b72372e6b17d82cacefabd22a
sidmohanty11 <sidmohanty11@gmail.com>
2024-08-28 18:41:58 +0530

    try?

--- vite.config.ts
+++ vite.config.ts
@@ -28,11 +28,11 @@ export default defineConfig(({ command, mode }): UserConfig => {
       // For example ['better-sqlite3'] if you use that in server functions.
       exclude: [],
     },
-    build: {
-      rollupOptions: {
-        external: ['isolated-vm']
-      }
-    },
+    // build: {
+    //   rollupOptions: {
+    //     external: ['isolated-vm']
+    //   }
+    // },
     /**
      * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
      */
